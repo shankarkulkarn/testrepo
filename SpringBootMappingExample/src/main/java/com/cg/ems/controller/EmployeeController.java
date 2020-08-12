2ab37f3a670b37c718613d68ae1722436b1db579
shankar <shankarkulkarn@gmail.com>
2020-08-12 17:01:58 +0530

    commit

--- SpringBootMappingExample/src/main/java/com/cg/ems/controller/EmployeeController.java
+++ SpringBootMappingExample/src/main/java/com/cg/ems/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.cg.ems.exception.EmployeeException;
 import com.cg.ems.service.EmployeeService;
 
 @RestController
+@CrossOrigin("*")
 public class EmployeeController {
 	
 	@Autowired
